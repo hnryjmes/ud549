283c6befea255e80d349481ced907ceeea6e7e0b
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-28 09:33:15 +0100

    new spec

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,10 +1,18 @@
 describe('Address Book', function() {
   it('should be able to add a contact', function() {
-    var addressBook = new AddressBook();
+    var addressBook = new AddressBook(),
     thisContact = new Contact();
 
     addressBook.addContact(thisContact);
 
     expect(addressBook.getContact(0)).toBe(thisContact);
+  });
+
+  it('should be able to delete a contact', function() {
+    var addressBook = new AddressBook(),
+    thisContact = newContact;
+
+    addressBook.addContact(thisContact);
+    addressBook.deleteContact(0);
   });
 });
